dc8aef1384483cf747321d0ba1a7d6ee15069610
SarCasm <sarcasm008@gmail.com>
2016-12-08 23:48:16 +0200

    Gem Initialized

---
+++ lib/i18n/lazy/generator.rb
@@ -0,0 +1,9 @@
+require "i18n/lazy/generator/version"
+
+module I18n
+  module Lazy
+    module Generator
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/i18n/lazy/generator_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe I18n::Lazy::Generator do
+  it 'has a version number' do
+    expect(I18n::Lazy::Generator::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
