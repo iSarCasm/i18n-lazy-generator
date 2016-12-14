36cfbfd632f5d233dd3f2d6f3c500f2cc7e5250d
SarCasm <sarcasm008@gmail.com>
2016-12-15 01:28:45 +0200

    Key Name Generator implemented

--- lib/i18n-lazy-generator/generator/key_name_generator.rb
+++ lib/i18n-lazy-generator/generator/key_name_generator.rb
@@ -1,19 +1,22 @@
 module I18n::Lazy::Generator
   module KeyName
     def self.generate(content)
-      # name = content.gsub(/%\S+/, "")
-      # name = name.gsub("'", "").gsub("`", "")
-      # name = name.split(' ').first(5).join(' ')
-      key_name = content.downcase.gsub(/\W+/, '_')
-      # key_name = content[0...content.size-1] if content[-1] == '_'
-      # name << '_html' if contains_html?(content)
+      contains_html = contains_html?(content)
+      key_name = content.downcase           # downcase
+      key_name = remove_html(key_name)
+      key_name.gsub!(/\s+/, '_')            # substitute spaces
+      key_name.gsub!(/\W+/,'')              # remove special chars
+      key_name = key_name.split('_').first(5).join('_') # leave only 5 first words
+      key_name += '_html' if contains_html
       key_name
     end
 
-    # def self.contains_html?(text)
-    #   text.split('').any? do |x|
-    #     x == '%'
-    #   end
-    # end
+    def self.contains_html?(text)
+      text =~ /<.+?>/
+    end
+
+    def self.remove_html(text)
+      text.gsub(/<.+?>/,'')
+    end
   end
 end
--- spec/i18n-lazy-generator/key_name_generator_spec.rb
+++ spec/i18n-lazy-generator/key_name_generator_spec.rb
@@ -11,14 +11,44 @@ describe I18n::Lazy::Generator::KeyName do
     },
     {
       input:    "it's 15% rainy today!",
-      output:   "it_is_rainy_today",
+      output:   "its_15_rainy_today",
       comment:  "text with some special chars"
+    },
+    {
+      input:    "its 15 rainy today. I am so happy about it. Gonna tell all my friends this crap.",
+      output:   "its_15_rainy_today_i",
+      comment:  "long text (should cut to 5 first words)"
+    },
+    {
+      input:    "hi bro",
+      output:   "hi_bro",
+      comment:  "very short text"
+    },
+    {
+      input:    "Its <b>rainy</b> today",
+      output:   "its_rainy_today_html",
+      comment:  "text with html tags (should add _html to an end & no tags in key name)"
+    },
+    {
+      input:    "Its <b>rainy</b> today",
+      output:   "its_rainy_today_html",
+      comment:  "text with html tags (should add _html to an end & no tags in key name)"
+    },
+    {
+      input:    "<div class='col-md-1.5'>Its <b>rainy</b> today</div><em><br></em>",
+      output:   "its_rainy_today_html",
+      comment:  "text with html tags (should add _html to an end & no tags in key name)"
+    },
+    {
+      input:    "<div><div>yo</em><alpachino alt='wtf'>",
+      output:   "yo_html",
+      comment:  "text with html tags (should add _html to an end & no tags in key name)"
     }
   ]
 
   describe '.generate' do
     tests.each do |test|
-      it "generates name correctly from #{test[:comment]}" do
+      it "works correctly with #{test[:comment]}" do
         expect(subject.generate(test[:input])).to eq(test[:output])
       end
     end
