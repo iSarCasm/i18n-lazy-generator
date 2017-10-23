59f54d0bcc49c2b2259ee550e94f9fedd43e8775
SarCasm <sarcasm008@gmail.com>
2017-10-23 12:47:22 +0300

    Change top level api

--- lib/lazy-translate/translator.rb
+++ lib/lazy-translate/translator.rb
@@ -1,18 +1,19 @@
 module LazyTranslate
   module Translator
     def self.translate(source_path: nil, config_path: nil, context: nil)
-      read_source   = File.read(source_path)
-      source_format = ToFileType.from_path(source_path)
-      new_source    = SourceUpdater.update(source_path, source_format)
+      source_content  = File.read(source_path)
+      source_format   = ToFileType.from_path(source_path)
+      new_source      = SourceUpdater.update(source_path, source_format)
 
-      read_config   = File.read(config_path)
-      config_format = ToFileType.from_path(config_path)
-      new_config    = ConfigUpdater.update({}, config_path, config_format)
+      config_content  = File.read(config_path)
+      config_format   = ToFileType.from_path(config_path)
+      new_config      = ConfigUpdater.update({}, config_content, config_format)
 
       puts Diffy::Diff.new(read_source, new_source).to_s(:color)
       puts 'Want to save the changes? (y/n)'
-      File.open(source_path, 'w') { |f| f.write(new_source) } if $stdin.gets.chomp.downcase == 'y'
-      File.open(config_path, 'w') { |f| f.write(new_source) } if $stdin.gets.chomp.downcase == 'y'
+      confirmed_changes = $stdin.gets.chomp.downcase == 'y'
+      File.open(source_path, 'w') { |f| f.write(new_source) } if confirmed_changes
+      File.open(config_path, 'w') { |f| f.write(new_source) } if confirmed_changes
     end
   end
 end
