599276d73cebe620ee9940f3799445efb81d2b91
SarCasm <sarcasm008@gmail.com>
2017-10-23 14:45:14 +0300

    Refactor FileType spec

--- spec/lazy-translate/file_type_spec.rb
+++ spec/lazy-translate/file_type_spec.rb
@@ -1,51 +1,53 @@
 require 'spec_helper'
 
 describe LazyTranslate::FileType do
+  subject = LazyTranslate::FileType
+
   describe '#type' do
     describe 'returns file type as a String' do
       it do
-        expect(LazyTranslate::FileType.new('file.rb').type).to eq 'rb'
+        expect(subject.new('file.rb').type).to eq 'rb'
       end
 
       it do
-        expect(LazyTranslate::FileType.new('file.yml').type).to eq 'yml'
+        expect(subject.new('file.yml').type).to eq 'yml'
       end
 
       it do
-        expect(LazyTranslate::FileType.new('file.haml').type).to eq 'haml'
+        expect(subject.new('file.haml').type).to eq 'haml'
       end
 
       it do
-        expect(LazyTranslate::FileType.new('file.html.haml').type).to eq 'haml'
+        expect(subject.new('file.html.haml').type).to eq 'haml'
       end
 
       it do
-        expect(LazyTranslate::FileType.new('/some/.stuff/file.html.haml').type).to eq 'haml'
+        expect(subject.new('/some/.stuff/file.html.haml').type).to eq 'haml'
       end
     end
   end
 
   describe '#source_parser' do
     it do
-      expect(LazyTranslate::FileType.new('file.haml').source_parser).to eq LazyTranslate::SourceParserHAML
+      expect(subject.new('file.haml').source_parser).to eq LazyTranslate::SourceParserHAML
     end
   end
 
   describe '#config_parser' do
     it do
-      expect(LazyTranslate::FileType.new('file.yml').config_parser).to eq LazyTranslate::ConfigParserYML
+      expect(subject.new('file.yml').config_parser).to eq LazyTranslate::ConfigParserYML
     end
   end
 
   describe '#source_updater' do
     it do
-      expect(LazyTranslate::FileType.new('file.haml').source_updater).to eq LazyTranslate::SourceUpdaterHAML
+      expect(subject.new('file.haml').source_updater).to eq LazyTranslate::SourceUpdaterHAML
     end
   end
 
   describe '#config_updater' do
     it do
-      expect(LazyTranslate::FileType.new('file.yml').config_updater).to eq LazyTranslate::ConfigUpdaterYML
+      expect(subject.new('file.yml').config_updater).to eq LazyTranslate::ConfigUpdaterYML
     end
   end
 end
