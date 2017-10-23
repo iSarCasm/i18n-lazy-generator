99e036a5e3c532a93e1acd229b0dc1026df7f627
SarCasm <sarcasm008@gmail.com>
2017-10-23 15:25:39 +0300

    Placeholder class SourceParserHAML

--- lib/lazy-translate/parser/source_parser_haml.rb
+++ lib/lazy-translate/parser/source_parser_haml.rb
@@ -1,4 +1,7 @@
 module LazyTranslate
   class SourceParserHAML
+    def parse(content)
+      # Need to return TranslationElements
+    end
   end
 end
---
+++ spec/lazy-translate/parser/source_parser_haml_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe LazyTranslate::SourceParserHAML do
+  subject = LazyTranslate::SourceParserHAML
+
+  describe '#parse' do
+    before do
+      @haml_1 = <<-HAML
+        #content
+          .left.column
+            %h2 Welcome to our site!
+            %p= print_information
+          .right.column
+            = render :partial => "sidebar"
+      HAML
+    end
+
+    it 'converts HAML file to TranslationElements' do
+      expect(subject.parse(@haml_1)).to contain_exactly(
+        LazyTranslate::TextElement.new(content: "Welcome to our site!", start: 32, finish: 51),
+      )
+    end
+  end
+end
