937a8df1ae775cb33209d420456ea3600d7e7493
SarCasm <sarcasm008@gmail.com>
2018-02-04 21:55:23 +0200

    Code format

--- lib/lazy-translate/parsers/source_parser_haml.rb
+++ lib/lazy-translate/parsers/source_parser_haml.rb
@@ -2,7 +2,13 @@ module LazyTranslate
   class SourceParserHAML
     def parse(source_text)
       [
-        TranslationElement.new(original: 'Some Text', location: :text, line: 1, start: 2, finish: 12)
+        TranslationElement.new(
+          original: 'Some Text',
+          location: :text,
+          line: 1,
+          start: 2,
+          finish: 12
+        )
       ]
     end
   end
